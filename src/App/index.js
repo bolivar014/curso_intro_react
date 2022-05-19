17dda50bb583146cc07ce47ee81fe085c83a5482
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-05-19 08:49:35 -0500

    organización de archivos y carpetas | Estructura ReactJS

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,11 @@
 // Importamos libreria
 import React from 'react';
-import { TodoCounter } from './TodoCounter';
-import { TodoSearch } from './TodoSearch';
-import { CreateTodoButton } from './CreateTodoButton';
-import { TodoList } from './TodoList';
-import { TodoItem } from './TodoItem';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { CreateTodoButton } from '../CreateTodoButton';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { AppUI } from './AppUI';
 
 // Inicializamos variable constante todos
 const defaultTodos = [
@@ -95,32 +96,15 @@ function App() {
 
   // Retornamos
   return (
-    <React.Fragment>
-      <TodoCounter 
-        total={totalTodos}
-        completed={completedTodos}
-      />
-    
-      <TodoSearch 
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
-      
-      <TodoList>
-        {searchedTodos.map(todo => (
-          <TodoItem 
-            key={todo.text} 
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}  
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
-
-      <CreateTodoButton />
-      
-    </React.Fragment>
+    <AppUI 
+      totalTodos={totalTodos}
+      completedTodos={completedTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
 
