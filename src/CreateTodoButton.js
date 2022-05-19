87f3a3f6d333fad89e8c1a065f8f029b50ba88e2
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-05-18 20:05:49 -0500

    control de eventos - onClick - ReactJS

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -6,8 +6,19 @@ import './CreateTodoButton.css';
 
 // Creamos función CreateTodoButton
 function CreateTodoButton() {
+    // Creamos función constante para evento de click
+    const onClickButton = (msg) => {
+        alert(msg);
+    }
+
+    // Retornamos función
     return (
-        <button className='CreateTodoButton'>+</button>
+        <button 
+            className='CreateTodoButton'
+            onClick={() => onClickButton('recibio click!')}
+        >
+            +
+        </button>
     );
 }
 
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -6,13 +6,29 @@ import './TodoItem.css';
 
 // Creamos función TodoItem
 function TodoItem(props) {
+    // Función que determina tareas completadas
+    const onComplete = () => {
+        alert('Ya completaste el todo ' + props.text);
+    };
+
+    // Función que determina tareas incompletas
+    const onDelete = () => {
+        alert('Borraste el todo ' + props.text);
+    };
+
     return (
         <li className='TodoItem'>
-            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>√</span>
+            <span 
+                className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+                onClick={onComplete}
+            >√</span>
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
                 {props.text}
             </p>
-            <span className="Icon Icon-delete">X</span>
+            <span 
+                className="Icon Icon-delete"
+                onClick={onDelete}
+            >X</span>
         </li>
     );
 }
