0c15b61b1fb1369307f79cdfa646c33914f3cf94
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-05-18 20:26:12 -0500

    manejo de estados | ReactJS

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -6,20 +6,27 @@ import './TodoSearch.css';
 
 // Creamos función TodoSearch
 function TodoSearch() {
+    // Inicializamos estado de React | Funciona por medio de funciones
+    const [searchValue, setSearchValue] = React.useState('');
+
     // Inicializamos función onSearchValueChange - con evento
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+
+        // Enviamos argumento digitado en el Input hacía la función setSearchValue
+        setSearchValue(event.target.value);
     };
 
     // Retornamos función
-    return (
+    return [
         <input 
             className='TodoSearch' 
             placeholder="Cebolla"
+            value={searchValue}
             onChange={onSearchValueChange}
-        />
-      
-    );
+        />,
+        <p>{searchValue}</p>
+    ];
 }
 
 // Exportamos modulo
